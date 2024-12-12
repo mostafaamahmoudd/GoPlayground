d2c468867b8effbd7a85f7a093eccc8260500c17
mostafaamahmoudd <mostafamahmoud805554@std.mans.edu.eg>
2024-12-12 13:50:13 +0200

    functions

--- basics/functions.go
+++ basics/functions.go
@@ -10,8 +10,15 @@ func plusplus(a, b, c int) int {
 	return a + b + c
 }
 
+func multipleVals() (int, int) {
+	return 1, 2
+}
+
 func functions() {
 	fmt.Println(sum(1, 2))
 
 	fmt.Println(plusplus(1, 2, 3))
+
+	a, b := multipleVals()
+	fmt.Println(a, b)
 }
